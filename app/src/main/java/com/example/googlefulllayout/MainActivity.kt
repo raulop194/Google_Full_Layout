3cb9de1339b06584f08f56f5af33de35bc0ff457
MarBenDAM2 <marben965@alu.centroafuera.es>
2023-02-03 14:04:11 +0100

    Todo hecho.

--- app/src/main/java/com/example/googlefulllayout/MainActivity.kt
+++ app/src/main/java/com/example/googlefulllayout/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.googlefulllayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.googlefulllayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val array = arrayListOf(binding.imageButton5, binding.imageButton7, binding.imageButton6, binding.CardView1, binding.CardView2, binding.CardView3, binding.imageButton3, binding.imageButton4)
+
+        for (i in array){
+            i.setOnClickListener {
+                val intent = Intent(this, ActivityEmptyTemp::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
